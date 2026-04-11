7bcf332bd9f21c5ec379615267d0d777f65e2292
zavora.dmitriy <zavora.dmitriy>
2026-04-11 15:34:00 +0300

    1.2.1

--- custom_components/elegoo_printer/__init__.py
+++ custom_components/elegoo_printer/__init__.py
@@ -3,8 +3,15 @@ from .const import DOMAIN
 PLATFORMS = ["sensor", "camera", "button"]
 
 async def async_setup_entry(hass, entry):
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_unload(entry, platform)
+        )
+    return True
