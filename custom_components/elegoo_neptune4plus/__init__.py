988e65b422dbd300b3083c41f61588eb39a4c50b
zavora.dmitriy <zavora.dmitriy>
2026-04-14 19:05:59 +0300

    2.0.2

--- custom_components/elegoo_neptune4plus/__init__.py
+++ custom_components/elegoo_neptune4plus/__init__.py
@@ -1,12 +1,17 @@
-from .const import DOMAIN
-from .sdcp import SDCPClient
+DOMAIN = "elegoo_neptune4plus"
+
+async def async_setup(hass, config):
+    return True
 
 async def async_setup_entry(hass, entry):
-    client = SDCPClient(entry.data['host'])
+    # ІМПОРТ ТІЛЬКИ ТУТ
+    from .sdcp import SDCPClient
+
+    client = SDCPClient(entry.data["host"])
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = client
 
-    await hass.config_entries.async_forward_entry_setup(entry, 'sensor')
-    await hass.config_entries.async_forward_entry_setup(entry, 'button')
-    await hass.config_entries.async_forward_entry_setup(entry, 'number')
-    await hass.config_entries.async_forward_entry_setup(entry, 'camera')
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setup(entry, "button")
+    await hass.config_entries.async_forward_entry_setup(entry, "number")
+    await hass.config_entries.async_forward_entry_setup(entry, "camera")
     return True
